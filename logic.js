494187a4e063dc69aace6a3ce0a2ad1bc6592931
Eric Lau <eric_l@mit.edu>
2016-04-28 21:58:00 -0400

    Initial commit.

---
+++ logic.js
@@ -0,0 +1,13 @@
+/* Javascript logic for video playback and overlays */
+
+$(document).ready(function() {
+	//factOverlay 
+	var items = ["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8"];
+	var delays = [19000, 32000, 45000, 58000, 71000, 84000, 97000, 110000];
+
+	$('#mapOverlay').delay(delays[0]).fadeIn().delay(107000).fadeOut();
+
+	for (var i = 0; i < items.length; i++) {
+		$("#" + items[i]).delay(delays[i]).fadeIn(200).delay(12500).fadeOut(200);
+	} 
+}); 

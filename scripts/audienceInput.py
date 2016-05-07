039d7db52d952e7dfc35017318dff1ff6bf14799
Eric Lau <eric_l@mit.edu>
2016-05-07 16:10:32 -0400

    Add other video, audio input logic.

---
+++ scripts/audienceInput.py
@@ -0,0 +1,29 @@
+# Code to process microphone input for audience data
+
+import pyaudio
+import wave
+import audioop
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+while True: 
+	data = stream.read(CHUNK)
+	rms = audioop.rms(data, 2) 
+	print "rms = ", rms
+
+stream.stop_stream()
+stream.close()
+p.terminate()
